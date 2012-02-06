d11dbb5dd9c238cc3e50d4bc7c09b63655171d45
Ranjan <mail@priyaranjan.net>
2012-02-06 12:57:42 +0530

    cleanup code
    code while working on test env to transferring online was left
    unchanged in few aspects directly affecting how code appears.

--- fetchCurrentSong.php
+++ fetchCurrentSong.php
@@ -1,13 +1,12 @@
 <?php
-$file = file_get_contents("http://www.181.fm/station_playing/181-themix.html");
-echo $file;
+    $file = file_get_contents("http://www.181.fm/station_playing/181-themix.html");
+    echo $file;
 ?>
 <script type="text/javascript">
 var trackinfo = document.getElementsByTagName('font')[2].innerHTML;
 var trackInfoRaw = trackinfo.split(/\s\-\s/);
 var artist = trackInfoRaw[0];
 var trackName = trackInfoRaw[1];
-//window.parent.document.getElementById('trackinfomaster').innerHTML = artist+' - '+trackName;
 window.parent.document.getElementById('artist').innerHTML = artist;
 window.parent.document.getElementById('trackName').innerHTML = trackName;      
 </script>
